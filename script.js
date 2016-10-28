69beacb087d7dac95c39b679946d84316dc69a6e
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-28 08:44:54 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 console.log("time to add some firebase code!")
 
-var key1 = {
-  nestedKey1: "hi!",
-  nestedKey2: "bye!"
+var key2 = {
+  nestedKey1:  
 }
 
-storeValue(key1, "Montana")
+storeValue("key1", key1)
