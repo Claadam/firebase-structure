39911ecfd45c2411e3caf6e5cd1a914fd6f2a06b
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-28 08:58:29 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 console.log("time to add some firebase code!")
 
-//var notAsNestedKey = {
-// notAsNestedKey: "I'm not as nested!" 
-//}
-//
-//var ReallyNestedKey = {
-//  ReallyNestedKey: "I'm deeply nested!"
-//}
-//
-//var nestedKey4 = {
-// nestedKey4: ReallyNestedKey 
-//}
-//
-//var nestedKey2 = {
-//  nestedKey5: notAsNestedKey,
-//  nestedkey3: nestedKey4
-//}
-//
-//var nestedKey1 = {
-//  nestedKey2: nestedKey2
-//}
-//
-//var key2 = {
-//  nestedKey1:  nestedKey1
-//}
+var key1 = {
+ nestedKey1: "hi!" 
+}
 
-//storeValue("key2", key2)
+storeValue("key1", key1)
+
+var notAsNestedKey = {
+ notAsNestedKey: "I'm not as nested!" 
+}
+
+var ReallyNestedKey = {
+  ReallyNestedKey: "I'm deeply nested!"
+}
+
+var nestedKey4 = {
+ nestedKey4: ReallyNestedKey 
+}
+
+var nestedKey2 = {
+  nestedKey5: notAsNestedKey,
+  nestedkey3: nestedKey4
+}
+
+var nestedKey1 = {
+  nestedKey2: nestedKey2
+}
+
+var key2 = {
+  nestedKey1:  nestedKey1
+}
+
+storeValue("key2", key2)
 
 storeValue("key3", "just me!")
 
